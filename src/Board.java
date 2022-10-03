3429514090b3fb4c6214c36ea4aad2b191db43ba
frida <yhma22.nilsfr@folkuniversitetet.nu>
2022-10-03 13:00:16 +0200

    1. Created 2 new classes, board and player. 2. Created the board and a method to print it out. next is to make 2 players and make them able to place out a symbol X/O to the board. 3. created a method called placesymbol to make the players being able to place out a symbol. now we just need to make the players position to store in it. 4. In my player class I created two methods "player1Move" & "player2Move" for each player that do practically the same thing. I also created a new method in board that will check if the player has won. 5. The board printed out several times with the playermove methods. Changed it to only one method "playerMove". Also added the println for names into the playerMove. 6. The game works alright. I need to make a function that doesnt allow the players to place a symbol where it already is a symbol. Also i want the game to proceed after its done. 7. Made a method that will check if a square is taken by the other player.

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Board {
+
+
 
     // A 2D-array, which will be our board/grid for our tictactoe-game.
 
@@ -21,9 +27,66 @@ public class Board {
         }
     }
 
-    //
+    public void placeSymbol(char[][] gameBoard, int pos, String player){
+
+        char symbol = ' ';
+
+        if(player.equals("player1")) {
+            symbol = 'X';
+            TicTacToe.playerPosition1.add(pos);
+        } else if (player.equals("player2")){
+            symbol = 'O';
+            TicTacToe.playerPosition2.add(pos);
+        }
+
+        switch (pos) {
+            case 1 -> gameBoard[0][0] = symbol;
+            case 2 -> gameBoard[0][2] = symbol;
+            case 3 -> gameBoard[0][4] = symbol;
+            case 4 -> gameBoard[1][0] = symbol;
+            case 5 -> gameBoard[1][2] = symbol;
+            case 6 -> gameBoard[1][4] = symbol;
+            case 7 -> gameBoard[2][0] = symbol;
+            case 8 -> gameBoard[2][2] = symbol;
+            case 9 -> gameBoard[2][4] = symbol;
+        }
+    }
+    public static String checkWinner(String player1, String player2){    // To get the names from the players I will put in their names in the checkwinner method.
+
+        List<Integer> topRow = Arrays.asList(1, 2, 3);
+        List<Integer> midRow = Arrays.asList(4, 5, 6);
+        List<Integer> botRow = Arrays.asList(7, 8, 9);
+        List<Integer> leftCol = Arrays.asList(1, 4, 7);
+        List<Integer> midCol = Arrays.asList(2, 5, 8);
+        List<Integer> rightCol = Arrays.asList(3, 6, 9);
+        List<Integer> cross1 = Arrays.asList(1, 5, 9);
+        List<Integer> cross2 = Arrays.asList(3, 5, 7);
+
+        ArrayList<List> winning = new ArrayList<List>();
+        winning.add(topRow);
+        winning.add(midRow);
+        winning.add(botRow);
+        winning.add(leftCol);
+        winning.add(midCol);
+        winning.add(rightCol);
+        winning.add(cross1);
+        winning.add(cross2);
 
 
-
-
+        for (List l : winning) {
+            if (TicTacToe.playerPosition1.containsAll(l)) {
+                return "Congratulations " + player1 + " you won! ";
+            } else if (TicTacToe.playerPosition2.containsAll(l)) {
+                return "Congratulations " + player2 + " you won! ";
+            }
+        }
+        if (TicTacToe.playerPosition1.size() + TicTacToe.playerPosition2.size() == 9) {
+            return "It's a draw!";
+        }
+        return "";
+    }
 }
+
+
+
+
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,8 +1,26 @@
+import java.util.ArrayList;
+
 public class TicTacToe {
+
+    static ArrayList<Integer> playerPosition1 = new ArrayList<>();
+    static ArrayList<Integer> playerPosition2 = new ArrayList<>();
+
     public static void main(String[] args) {
 
-        Board board = new Board();
-        board.printBoard();
+
+            introMenu();
+            Player.playerMove();
+
+
+    }
+
+    public static void introMenu(){
+
+        System.out.println("*===================================*");
+        System.out.println("Hello & welcome to my TicTacToe-game!");
+        System.out.println("How to play: Enter a number (1-9) and it will place an X/O in the square that you'll choose. But first enter your names! ");
+        System.out.println("*===================================*");
+
 
     }
 }
