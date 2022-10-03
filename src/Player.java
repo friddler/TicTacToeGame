dfb43762c7819bbccf46fbb1d67b4a8115f234dd
frida <yhma22.nilsfr@folkuniversitetet.nu>
2022-10-03 16:38:40 +0200

    1. Created 2 new classes, board and player. 2. Created the board and a method to print it out. next is to make 2 players and make them able to place out a symbol X/O to the board. 3. created a method called placesymbol to make the players being able to place out a symbol. now we just need to make the players position to store in it. 4. In my player class I created two methods "player1Move" & "player2Move" for each player that do practically the same thing. I also created a new method in board that will check if the player has won. 5. The board printed out several times with the playermove methods. Changed it to only one method "playerMove". Also added the println for names into the playerMove. 6. The game works alright. I need to make a function that doesnt allow the players to place a symbol where it already is a symbol. Also i want the game to proceed after its done. 7. Made a method that will check if a square is taken by the other player. 8. the while-loop works to check if the position is taken. 9. The game can continue within the while-loop in main but the second round it only prints out that the positions are taken.

--- src/Player.java
+++ src/Player.java
@@ -2,62 +2,65 @@ import java.util.Scanner;
 
 public class Player {
 
-
     public static void playerMove() {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter player 1 name: ");
-        String player1 = sc.nextLine();
-        System.out.println("Enter player 2 name: ");
-        String player2 = sc.nextLine();
+            Scanner sc = new Scanner(System.in);
 
-        Board board = new Board();
-        board.printBoard();
+            System.out.println("Enter player 1 name: ");
+            String player1 = sc.nextLine();
+            System.out.println("Enter player 2 name: ");
+            String player2 = sc.nextLine();
 
-        // *===========================================* Game begins
+            Board board = new Board();
+            board.printBoard();
+
+
+            // *===========================================* Game begins
+
         while (true) {
-            System.out.println(player1 + " enter your placement (1-9): ");
-            int playerPos1 = sc.nextInt();
-            while(TicTacToe.playerPosition1.contains(playerPos1) || TicTacToe.playerPosition2.contains(playerPos1)){
-                System.out.println("Position taken. Try again: ");
-                playerPos1 = sc.nextInt();
-            }
 
-            board.placeSymbol(board.gameBoard, playerPos1, "player1");
-            board.printBoard();
+                System.out.println(player1 + " enter your placement (1-9): ");
+                int playerPos1 = sc.nextInt();
+                while (TicTacToe.playerPosition1.contains(playerPos1) || TicTacToe.playerPosition2.contains(playerPos1)) {
+                    System.out.println("Position taken. Try again: ");
+                    playerPos1 = sc.nextInt();
+                }
 
-            String result = Board.checkWinner(player1, player2);
+                board.placeSymbol(board.gameBoard, playerPos1, "player1");
+                board.printBoard();
 
-            if (result.length() > 0) {
+                String result = Board.checkWinner(player1, player2);
+
+                if (result.length() > 0) {
+                    System.out.println(result);
+                    break;
+                }
+
+                // *===========================================* Player 2 makes a move.
+
+                System.out.println(player2 + " enter your placement (1-9): ");
+                int playerPos2 = sc.nextInt();
+                while (TicTacToe.playerPosition1.contains(playerPos2) || TicTacToe.playerPosition2.contains(playerPos2)) {
+                    System.out.println("Position taken. Try again: ");
+                    playerPos2 = sc.nextInt();
+                }
+
+                board.placeSymbol(board.gameBoard, playerPos2, "player2");
+                board.printBoard();
+
+                if (result.length() > 0) {
+                    System.out.println(result);
+                    break;
+                }
+
                 System.out.println(result);
-                break;
-            }
-
-            // *===========================================* Player 2 makes a move.
-
-            System.out.println(player2 + " enter your placement (1-9): ");
-            int playerPos2 = sc.nextInt();
-            while(TicTacToe.playerPosition1.contains(playerPos2) || TicTacToe.playerPosition2.contains(playerPos2)){
-                System.out.println("Position taken. Try again: ");
-                playerPos2 = sc.nextInt();
-            }
-
-            board.placeSymbol(board.gameBoard, playerPos2, "player2");
-            board.printBoard();
-
-            if (result.length() > 0) {
-                System.out.println(result);
-                break;
-            }
-
-            System.out.println(result);
-
         }
-
-
     }
-
 }
+
+
+
+
 
 
 
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -9,16 +9,29 @@ public class TicTacToe {
 
 
             introMenu();
-            Player.playerMove();
+            while (true) {
+                Player.playerMove();
+            }
+
 
 
     }
+
+
+
 
     public static void introMenu(){
 
         System.out.println("*===================================*");
         System.out.println("Hello & welcome to my TicTacToe-game!");
-        System.out.println("How to play: Enter a number (1-9) and it will place an X/O in the square that you'll choose. But first enter your names! ");
+        System.out.println("How to play: Enter a number (1-9) and it will place an X/O in the square that you'll choose.");
+        System.out.println("*==============================*");
+        System.out.println("{'1', '|', '2', '|', '3'},\n" +
+                "{'4', '|', '5', '|', '6'},\n" +
+                "{'7', '|', '8', '|', '9'},");
+        System.out.println("*==============================*");
+        System.out.println("Player 1 is X, Player 2 is O.");
+        System.out.println("Now, enter your names! Good luck! ");
         System.out.println("*===================================*");
 
 
